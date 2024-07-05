61e720ddc83397a694d8e5928e8366c0b82fab7b
1Andrey18 <Chike399@mail.ru>
2024-07-05 23:23:01 +1000

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <GitHubProfiles />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GitHubProfiles from './components/GitHubProfiles.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GitHubProfiles
   }
-}
+};
 </script>
 
 <style>
